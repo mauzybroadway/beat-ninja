5b9dff738a5e7dd40b3f5938a144501b6ca540b9
Brandon Wetzel <a0226847@ti.com>
2018-02-25 14:50:47 -0500

    android studio project all set up

---
+++ app/src/main/java/ga/mauzy/beatninja/BigWaveEditor.java
@@ -0,0 +1,13 @@
+package ga.mauzy.beatninja;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BigWaveEditor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_big_wave_editor);
+    }
+}
